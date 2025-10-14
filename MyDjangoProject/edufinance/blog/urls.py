996456ebd9ab2e979b03e4387175a21f8c6272f0
Adewale <adewale.oladiti28@gmail.com>
2025-10-14 17:27:39 +0100

    Enhance blog functionality by adding post list view and updating URLs; removed deprecated home template.

--- MyDjangoProject/edufinance/blog/urls.py
+++ MyDjangoProject/edufinance/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, post_list
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
+    path('posts', post_list, name='posts')
 ]
--- MyDjangoProject/edufinance/blog/views.py
+++ MyDjangoProject/edufinance/blog/views.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -11,4 +12,17 @@ from django.http import HttpResponse
 
 def home(request):
 
-    return render(request, 'home.html',  {'name': 'James'})
+    return render(request, 'pages/home.html',  {'name': 'James'})
+
+def post_list(request):
+    posts = [
+        {'title': 'Presidential Election in Nigeria', 'body': "There is an alarming rate of corruption during the presidential election in Nigeria"},
+        {'title': 'Organised Crimes around Lagos', 'body': 'It was recorded that over the decades, we have at least 50 victims of organised crimes per year in Lagos state'},
+        {'title': 'Presidential Election in Nigeria', 'body': "There is an alarming rate of corruption during the presidential election in Nigeria"},
+        {'title': 'Organised Crimes around Lagos', 'body': 'It was recorded that over the decades, we have at least 50 victims of organised crimes per year in Lagos state'},
+        {'title': 'Presidential Election in Nigeria', 'body': "There is an alarming rate of corruption during the presidential election in Nigeria"},
+        {'title': 'Organised Crimes around Lagos', 'body': 'It was recorded that over the decades, we have at least 50 victims of organised crimes per year in Lagos state'},
+        {'title': 'Hackaton Projects in Appclick Academy', 'body': 'We have two week starting from next week to carry out our hackaton project'},
+    ]
+
+    return render(request, 'pages/posts.html', {'posts': posts})
