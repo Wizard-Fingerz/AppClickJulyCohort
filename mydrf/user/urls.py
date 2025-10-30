6fe1743366d6a9d3dd148e617de5950d6ab32010
Adewale <adewale.oladiti28@gmail.com>
2025-10-30 17:23:25 +0100

    Add token authentication to user app; enhance StudentViewSet with profile and top_students actions

--- mydrf/user/urls.py
+++ mydrf/user/urls.py
@@ -1,9 +1,15 @@
 
 from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from rest_framework.authtoken import views
 from .views import StudentViewSet
 
 router = DefaultRouter()
 router.register(r'students', StudentViewSet, basename='student')
 
+urlpatterns = [
+    path('api-token-auth/', views.obtain_auth_token, name='obtain_auth_token'),
+]
 
-urlpatterns = router.urls
+# Include both the router URLs and the token auth endpoint
+urlpatterns += router.urls
--- mydrf/user/views.py
+++ mydrf/user/views.py
@@ -2,8 +2,22 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Student
 from .serializers import StudentSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 # Create your views here.
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+    
+    @action(detail=True, methods=['get'])
+    def profile(self, request, pk=None):
+        student = self.get_object()
+        return Response({'name': student.name, 'course': student.course})
+
+    @action(detail=False, methods=['get'])
+    def top_students(self, request):
+        top_students = Student.objects.filter(age__gt=20)
+        serializer = self.get_serializer(top_students, many=True)
+        return Response(serializer.data)
